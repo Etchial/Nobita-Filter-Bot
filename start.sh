617f8e967a9b68e28bf17fc043cc3e27f01c290a
Etchial <98379633+Etchial@users.noreply.github.com>
2024-03-05 13:39:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Ethical/Nobita-Filter-Bot/tree/advance.git /Nobita-Filter-Bot
+  git clone https://github.com/Etchial/Nobita-Filter-Bot.git /Nobita-Filter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Nobita-Filter-Bot
